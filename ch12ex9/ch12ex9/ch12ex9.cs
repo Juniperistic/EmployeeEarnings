a0769b28ab7841f7a9c97586293fd2074f0287e2
Miranda Morris <94880693+Juniperistic@users.noreply.github.com>
2023-07-15 19:29:24 -0400

    Upload of Employee Earnings

---
+++ ch12ex9/ch12ex9/ch12ex9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*************************************************************************/
+/* Program Name:     Ch12Ex9.cs                                          */
+/* Date:             05/01/2022                                          */
+/* Programmer:       Miranda Morris                                      */
+/* Class:            CSCI 234                                            */
+/*                                                                       */
+/* Program Description: This code is meant to show the earnings of each  */
+/* employee as well as give a bonus $100 if it is within that employee's */
+/* birth month.                                                          */
+/*                                                                       */
+/* Input:	Names, SSN, birthday                                         */
+/*                                                                       */
+/* Output:	Name, SSN, earnings                                          */
+/*                                                                       */
+/* Givens:	Money based on sales, salary, or hours worked                */
+/*                                                                       */
+/* Testing Data: A list of employees as well as data related to them is  */
+/* outputted such as their ssn, weekly salary, earned, hours worked,     */
+/* commission raete, and gross sales.The birthday is not included        */
+/* or the bonus.                                                         */
+/*************************************************************************/
+
+namespace ch12ex9
+{
+    class ch12ex9
+    {
+       // static void Main(string[] args)
+        //{
+          //  Console.WriteLine("Hello World!");
+        //}
+    }
+}
